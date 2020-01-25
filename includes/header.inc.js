cff7f574ff0724bb930072a2a50e7ff4a4c44159
Nick <manelliproductions@gmail.com>
2020-01-24 19:24:08 -0500

    Starting a website redesign

--- includes/header.inc.js
+++
@@ -1,9 +0,0 @@
-function toggle() {
-    var links = document.getElementById("links");
-
-    if(links.classList.contains("moved")) {
-        links.classList.remove("moved");
-    } else {
-        links.classList.add("moved");
-    }
-}
