81b48ab857d2be1eb4ef30fafdb20873cf9bbf9d
Nick-Fanelli <nickfanelli5@gmail.com>
2021-04-01 14:23:33 -0400

    remove all old schedule information and implement feature for if schedule is empty

--- js/schedule.js
+++ js/schedule.js
@@ -1,49 +1,46 @@
-var tableTT = $("#schedule-top-three");
+var table = $("#schedule-all");
+
 
 function load() {
     fetch("../client/schedule.json").then(function(resp) {
         return resp.json();
     }).then(function(data) {
-        fillOutTableThree(data);
+        fillOutTable(data);
     });
 }
 
-function fillOutTableThree(data) {
+
+function fillOutTable(data) {
     var todayRaw = new Date();
     var today = new Date(`${todayRaw.getMonth() + 1}/${todayRaw.getDate()}/${todayRaw.getFullYear()}`);
 
-    i = 0;
-    s = 0;
-    
     var itemCount = 0;
 
-    while(i < Object.keys(data).length) {
+    for(i = 0; i < Object.keys(data).length; i++) {
         if(data[i].length == 4) {
+
             var target = new Date(data[i][1]);
 
             if(target.getTime() >= today.getTime()) {
-                tableTT.append(`
+                itemCount++;
+                table.append(`
                 <tr>
                     <td>${data[i][0]}</td>
                     <td>${data[i][1]}</td>
                     <td>${data[i][2]}</td>
                     <td class="${data[i][3] === "TBD" ? "tbd-place" : "place"}" ${data[i][3] === "TBD" ? "" : 'onclick="findSchool(this);"'}>${data[i][3]}</td>
                 </tr>`);
-                s++;
-
-                itemCount++;
-            }   
-            if(s >= 3) {
-                break;
             }
         }
-        i++;
     }
 
     if(itemCount <= 0) {
-        document.querySelector("div#table-ext").innerHTML = '<h1 id="table-no-events">No Upcoming Events</h1>';
+        document.querySelector("#schedule-all").classList.add("change-margin-bottom");
+        document.querySelector(".table-container").innerHTML += `
+        <h1 id="table-no-events" style="text-align: center; ">No Upcoming Events</h1>
+        `;
     }
-} 
+}
 
 function findSchool(text) {
     window.open(`https://www.google.com/maps/search/${text.innerHTML}, NJ`);
