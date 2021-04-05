66d187b72ec6adc4abef21c1c81964b4341e6730
Nick-Fanelli <nickfanelli5@gmail.com>
2021-04-04 22:53:25 -0400

    Finished NavBar

---
+++ js/nav.js
@@ -0,0 +1,15 @@
+const NavBurger = document.querySelector("nav div.burger");
+const NavbarCollapse = document.querySelector("nav div.navbar-collapse");
+
+function HandleButtonEvents() {
+    NavBurger.onclick = function() {
+        NavBurger.classList.toggle("open");
+        NavbarCollapse.classList.toggle("collapse");
+    }
+}
+
+window.onload = function() {
+
+    HandleButtonEvents();
+
+} 
